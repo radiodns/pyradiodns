3b21cae5a2d29b8e28f421568b3ba3c9eeea3899
Nick Piggott <nick@piggott.eu>
2020-08-07 12:39:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
+if sys.version_info[0] < 3:
+      install_requires = ["dnspython<2.0"]
+else:
+      install_requires = ["dnspython>=2.0"]
+      
 setup(name='pyradiodns',
       version='0.1',
       description='A Python port of phpradiodns.',
       author='samstarling',
       url='http://github.com/radiodns/pyradiodns',
       packages=['pyradiodns', 'pyradiodns.radiodns'],
-      if sys.version_info[0] < 3:
-          install_requires = ["dnspython<2.0"]
-      else:
-          install_requires = ["dnspython>=2.0"]
+      install_requires=install_requires)
